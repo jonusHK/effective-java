74d725fa55d7d67c8ffcebab68ad087c932af6b7
jonusHK <positipman@gmail.com>
2023-07-04 18:56:04 +0900

    Add: 프로젝트 초기화

---
+++ src/main/java/com/dhkpo/effectivejava/chapter01/item01/Settings.java
@@ -0,0 +1,2 @@
+package com.dhkpo.effectivejava.chapter01.item01;public class Settings {
+}
---
+++ src/test/java/com/dhkpo/effectivejava/EffectiveJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhkpo.effectivejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EffectiveJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

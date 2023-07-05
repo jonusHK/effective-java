78c8287ea435bc02d0bb85fe72ea19002374a4f6
jonusHK <positipman@gmail.com>
2023-07-05 17:16:45 +0900

    Add: 인터페이스 default method

--- src/main/java/com/dhkpo/effectivejava/chapter01/item01/HelloService.java
+++ src/main/java/com/dhkpo/effectivejava/chapter01/item01/HelloService.java
@@ -4,11 +4,16 @@ public interface HelloService {
 
     String hello();
 
+    // java8 에 등장한 public static 메소드
     static HelloService of(String lang) {
         if (lang.equals("ko")) {
             return new KoreanHelloService();
         } else {
             return new EnglishHelloService();
         }
+    }
+
+    default String hi() {
+        return "hi";
     }
 }

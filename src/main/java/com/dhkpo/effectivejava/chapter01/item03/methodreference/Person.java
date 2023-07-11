04c7f30cdaab32151822b833645f47d6d69a0399
jonusHK <positipman@gmail.com>
2023-07-11 18:23:06 +0900

    Add: MethodReference

---
+++ src/main/java/com/dhkpo/effectivejava/chapter01/item03/methodreference/Person.java
@@ -0,0 +1,39 @@
+package com.dhkpo.effectivejava.chapter01.item03.methodreference;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Person {
+
+    LocalDate birthday;
+
+    public static int compareByAge(Person a, Person b) {
+        return a.birthday.compareTo(b.birthday);
+    }
+
+    public Person(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public int getAge() {
+        return LocalDate.now().getYear() - birthday.getYear();
+    }
+
+    public static void main(String[] args) {
+        List<Person> people = new ArrayList<>();
+        people.add(new Person(LocalDate.of(1991, 2, 20)));
+        people.add(new Person(LocalDate.of(1990, 9, 11)));
+        people.add(new Person(LocalDate.of(2021, 4, 15)));
+
+        people.sort(Person::compareByAge);
+
+        List<LocalDate> dates = new ArrayList<>();
+        dates.add(LocalDate.of(1991, 2, 20));
+        dates.add(LocalDate.of(1990, 9, 11));
+        dates.add(LocalDate.of(2021, 4, 15));
+
+        dates.stream().map(Person::new).collect(Collectors.toList());
+    }
+}
